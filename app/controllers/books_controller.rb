f0fe851320fd0f48403bebdb57ed364cdf027c28
yamamo0301 <101253886+yamamo0301@users.noreply.github.com>
2022-05-14 07:02:11 +0000

    [Finish]create

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,11 +3,23 @@ class BooksController < ApplicationController
     @book = Book.new
   end
 
-
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+     redirect_to book_path(@book.id)
+    else
+      render :index
+    end
+  end
 
   def show
   end
 
   def edit
+  end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
